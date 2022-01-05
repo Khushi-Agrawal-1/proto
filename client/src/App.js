4d1c11c05e9a646d9e6fce95d16612cba35b80f2
Khushi Agrawal <khushi.agrawal2019@vitstudent.ac.in>
2022-01-05 21:24:50 +0530

    router

--- client/src/App.js
+++ client/src/App.js
@@ -1,150 +1,17 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import React, {useEffect, useState } from 'react';
-import { Button, Typography } from 'antd';
-import { useCountInterval, useMutationObserver } from './hooks';
-import client from './hooks/socketClient';
-import { TIME_INTERVAL } from './constants';
-import xtype from 'xtypejs'
-// import { w3cwebsocket as W3CWebSocket } from "websocket";
+import React from 'react';
 
+import Chat from './home';
+import Join from './join';
 
-const { Title, Text } = Typography;
-// const client = new W3CWebSocket('ws://127.0.0.1:8000');
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-function App() {
-
-  // function onButtonClick = (value) => {
-  //   client.send(JSON.stringify({
-  //     type: "message",
-  //     msg: value                                                                                                                                                                                                                                                                                                                                  
-  //   }));
-  // }
-
-  useEffect(
-    () => {
-      client.onopen = () => {
-        console.log('WebSocket Client Connected');
-        client.send("Hello server!");
-      }; 
-
-      // client.onmessage = (message) => {
-      //   const dataFromServer = JSON.parse(message.data);
-      //   console.log('got reply', dataFromServer);
-      // }
-    }
-  );
-
-
-
-
-
-  // MUTATION OBSERVER
-  const [isCounterActive, setIsCounterActive] = useState(false);
-  const [isMutationObserverActive, setIsMutationObserverActive] = useState(
-    false
-  );
-
-  const count = useCountInterval(isCounterActive);
-  useMutationObserver(isMutationObserverActive, mutations => {
-    console.log(xtype(mutations), mutations);
-    client.send(mutations);
-  }
-    
-  );
-
-  const handleStartCounterClick = () => {
-    setIsCounterActive(true);
-  };
-
-  const handleStopCounterClick = () => {
-    setIsCounterActive(false);
-  };
-
-  const handleStartMutationObserverClick = () => {
-    setIsMutationObserverActive(true);
-  };
-
-  const handleStopMutationObserverClick = () => {
-    setIsMutationObserverActive(false);
-  };
-
-
-
+const App = () => {
   return (
-    <div className="App">
-      {/* <button onClick={() => this.onButtonClick("hello")}> send mssg</button> */}
-      {/* <useMutationObserver {...this.props} socket={client} /> */}
-
-          <div className="block blockFlex">
-            <Button
-              type="default"
-              onClick={
-                isCounterActive
-                  ? handleStopCounterClick
-                  : handleStartCounterClick
-              }
-            >
-              {isCounterActive ? 'Stop' : 'Start'} counter
-            </Button>
-            <Button
-              type="primary"
-              className="primaryButton"
-              onClick={
-                isMutationObserverActive
-                  ? handleStopMutationObserverClick
-                  : handleStartMutationObserverClick
-              }
-            >
-              {isMutationObserverActive ? 'Stop' : 'Start'} MutationObserver
-            </Button>
-          </div>
-          <div className="block">
-            <Title level={4} >
-              Counter is{' '}
-              <Text >
-                {isCounterActive ? 'on' : 'off'}
-              </Text>
-            </Title>
-            <Title level={4} >
-              MutationObserver is{' '}
-              <Text >
-                {isMutationObserverActive ? 'on' : 'off'}
-              </Text>
-            </Title>
-          </div>
- 
-          <Title data-count={count} id="observedNode" className="text" >
-            Current count: {count}
-          </Title>
-          {isCounterActive && (
-            <p>
-              <Text className="text">
-                Counter active Incremented every {TIME_INTERVAL / 1000} seconds
-              </Text>
-            </p>
-          )}
-          {isMutationObserverActive && (
-            <p>
-              <Text className="text">
-                MutationObserver active
-              </Text>
-            </p>
-          )}
-          {isMutationObserverActive &&
-            !isCounterActive && (
-              <p>
-                <Text className="text">
-                MutationObserver active, Counter is not active
-                </Text>
-              </p>
-            )}
-
-
-    </div>
-          
-
-    
+    <Router>
+      <Route path="/" exact component={Join} />
+      <Route path="/chat" component={Chat} />
+    </Router>
   );
-};
+}
 
 export default App;
